16ec2c03e80cbb0e84ac4be3af796f31dfd0c96c
Muhammad Ahmad <muhammadahmad@gmail.com>
2026-08-03 01:17:18 +0500

    Finalize backend CORS fix for Vercel preview origins

--- server/app.js
+++ server/app.js
@@ -59,13 +59,7 @@ app.use((req, res, next) => {
   if (req.method === "OPTIONS") return res.sendStatus(204);
 
   next();
-});
-
-/* -------------------------------------------------------------------------- */
-/*                                 Middlewares                                */
-/* -------------------------------------------------------------------------- */
-
-// Secure HTTP headers.
+})/ Secure HTTP headers.
 app.use(helmet());
 
 // Enable Cross-Origin Resource Sharing.
